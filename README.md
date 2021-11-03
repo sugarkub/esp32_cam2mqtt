168f9286f25a11c5775c93af10feb2ba95b7e6a1
ilwoong <ilwoong.jeong@gmail.com>
2021-11-03 22:48:54 +0900

    Change code to reduce power consumption

--- README.md
+++ README.md
@@ -1,41 +1,63 @@
-# cam2mqtt
+# esp32_cam2mqtt
 
 ![usecase_of_cam2mqtt](_photo/cam2mqtt.png)
 
-Take snapshot and send it to an mqtt topic using ESP32-CAM.
+원본 프로젝트는 suapapa님의 [esp32_cam2mqtt](https://github.com/suapapa/esp32_cam2mqtt){:target="_blank"}입니다.
 
-* Works well with HomeAssistant's [MQTT Camera](https://www.home-assistant.io/integrations/camera.mqtt/)
-* Use [esp-idf](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/#).
-* It takes one -or two- picture for a day and go to deep sleep.
-* Date and time of the photo taken is printed in. [sample](_photo/sample.png).
+* HomeAssistant의 [MQTT Camera](https://www.home-assistant.io/integrations/camera.mqtt/){:target="_blank"}를 이용하여 결과를 확인할 수 있습니다.
+* [esp-idf](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/#)를 사용하여 빌드합니다.
+* 하루에 한장의 사진을 찍도록 되어 있습니다.
+* 날짜와 시간이 사진에 함께 출력됩니다. [sample](_photo/sample.png).
 
-## requirements
+## 사전준비
 
-Install [esp-idf](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/#)
+- [esp-idf](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/#)
 
-Install [ESP32 Camera Driver](https://github.com/espressif/esp32-camera):
+- [ESP32 Camera Driver](https://github.com/espressif/esp32-camera):
+```bash
+$ cd $IDF_PATH/components
+$ git clone https://github.com/espressif/esp32-camera
+```
 
-    $ cd $IDF_PATH/components
-    $ git clone https://github.com/espressif/esp32-camera
+## 환경설정 및 빌드
 
-## clone, configure and build
+소스코드를 다운로드합니다.
 
-Clone poject:
+```bash
+$ git clone https://github.com/sugarkub/esp32_cam2mqtt
+```
 
-    $ git clone https://github.com/suapapa/esp32_cam2mqtt
+ESP-IDF Console에서 wifi-ssid, wifi-pass, mqtt-uri, mqtt-topic 등을 설정하기 위해 menuconfig를 실행합니다.
 
-Configure wifi-ssid, wifi-pass, mqtt-uri, mqtt-topic and etc...:
+```bash
+$ idf.py menuconfig
+```
 
-    $ cd cam2mqtt
-    $ get_idf
-    $ idf.py menuconfig
+다음 내용을 설정합니다.
+- Wifi Configuration: SSID 및 패스워드 설정
+- MQTT Configuration: MQTT 서버 주소 설정
+- Component Config > ESP32-specific > Support for external, SPI-connected RAM 체크
 
-Build, flash, monitor:
+아래 명령어로 빌드할 수 있습니다.
 
-    $ idf.py build
-    $ idf.py -p /dev/ttyUSB0 flash
-    $ idf.py -p /dev/ttyUSB0 monitor
+```bash
+$ idf.py build
+$ idf.py -p COM4 flash
+$ idf.py -p COM4 monitor
+```
 
-## beautify code
+## Trouble shooting
 
-    indent -linux main/main.c
+X.509 인증서 관련해서 다음과 같은 빌드 오류가 나는 경우가 있습니다.
+
+```bash
+FAILED: esp-idf/mbedtls/x509_crt_bundle
+```
+
+아래와 같이 해결할 수 있습니다.
+
+```bash
+$ idf.py menuconfig
+```
+
+- Component Config > mbedTLS > Certificate Bundle > Default certificate bundle options > Use only the most certificates from the default bundles
--- main/main.c
+++ main/main.c
@@ -47,135 +47,135 @@ static const char *TAG = "cam2mqtt";
 
 static int s_retry_num = 0;
 
-RTC_DATA_ATTR static int take_count = 0;
+RTC_DATA_ATTR static int seq_num = 0;
+RTC_DATA_ATTR static int countdown = 0;
 
 static void
 wifi_event_handler(void *arg, esp_event_base_t event_base,
-		   int32_t event_id, void *event_data)
+           int32_t event_id, void *event_data)
 {
-	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-		esp_wifi_connect();
-	} else if (event_base == WIFI_EVENT
-		   && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-		if (s_retry_num < WIFI_MAX_RETRY) {
-			esp_wifi_connect();
-			s_retry_num++;
-			ESP_LOGI(TAG, "retry to connect to the AP");
-		} else {
-			xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-		}
-		ESP_LOGI(TAG, "connect to the AP fail");
-	} else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-		ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
-		ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-		s_retry_num = 0;
-		xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-	}
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    }
+	else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < WIFI_MAX_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        }
+		else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG, "connect to the AP fail");
+    }
+	else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
 }
 
 void wifi_init_sta(void)
 {
-	s_wifi_event_group = xEventGroupCreate();
+    s_wifi_event_group = xEventGroupCreate();
 
-	ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_netif_init());
 
-	ESP_ERROR_CHECK(esp_event_loop_create_default());
-	esp_netif_create_default_wifi_sta();
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
 
-	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-	esp_event_handler_instance_t instance_any_id;
-	esp_event_handler_instance_t instance_got_ip;
-	ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-							    ESP_EVENT_ANY_ID,
-							    &wifi_event_handler,
-							    NULL,
-							    &instance_any_id));
-	ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
-							    IP_EVENT_STA_GOT_IP,
-							    &wifi_event_handler,
-							    NULL,
-							    &instance_got_ip));
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                ESP_EVENT_ANY_ID,
+                                &wifi_event_handler,
+                                NULL,
+                                &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                IP_EVENT_STA_GOT_IP,
+                                &wifi_event_handler,
+                                NULL,
+                                &instance_got_ip));
 
-	wifi_config_t wifi_config = {
-		.sta = {
-			.ssid = WIFI_SSID,
-			.password = WIFI_PASS,
-			/* Setting a password implies station will connect to all security modes including WEP/WPA.
-			 * However these modes are deprecated and not advisable to be used. Incase your Access point
-			 * doesn't support WPA2, these mode can be enabled by commenting below line */
-			.threshold.authmode = WIFI_AUTH_WPA2_PSK,
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+            /* Setting a password implies station will connect to all security modes including WEP/WPA.
+             * However these modes are deprecated and not advisable to be used. Incase your Access point
+             * doesn't support WPA2, these mode can be enabled by commenting below line */
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
 
-			.pmf_cfg = {
-				    .capable = true,
-				    .required = false},
-			},
-	};
-	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-	ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-	ESP_ERROR_CHECK(esp_wifi_start());
+            .pmf_cfg = {
+                    .capable = true,
+                    .required = false},
+            },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
 
-	ESP_LOGI(TAG, "wifi_init_sta finished.");
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
 
-	/* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-	 * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
-	EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-					       WIFI_CONNECTED_BIT |
-					       WIFI_FAIL_BIT,
-					       pdFALSE,
-					       pdFALSE,
-					       portMAX_DELAY);
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                           WIFI_CONNECTED_BIT |
+                           WIFI_FAIL_BIT,
+                           pdFALSE,
+                           pdFALSE,
+                           portMAX_DELAY);
 
-	/* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
-	 * happened. */
-	if (bits & WIFI_CONNECTED_BIT) {
-		ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
-			 WIFI_SSID, WIFI_PASS);
-	} else if (bits & WIFI_FAIL_BIT) {
-		ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
-			 WIFI_SSID, WIFI_PASS);
-	} else {
-		ESP_LOGE(TAG, "UNEXPECTED EVENT");
-	}
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", WIFI_SSID, WIFI_PASS);
+    }
+	else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s", WIFI_SSID, WIFI_PASS);
+    }
+	else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
 
-	/* The event will not be processed after unregister */
-	ESP_ERROR_CHECK(esp_event_handler_instance_unregister
-			(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
-	ESP_ERROR_CHECK(esp_event_handler_instance_unregister
-			(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
-	vEventGroupDelete(s_wifi_event_group);
+    /* The event will not be processed after unregister */
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+    vEventGroupDelete(s_wifi_event_group);
 }
 
 void init_wifi(void)
 {
-	//Initialize NVS
-	esp_err_t ret = nvs_flash_init();
-	if (ret == ESP_ERR_NVS_NO_FREE_PAGES
-	    || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-		ESP_ERROR_CHECK(nvs_flash_erase());
-		ret = nvs_flash_init();
-	}
-	ESP_ERROR_CHECK(ret);
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 
-	ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
-	wifi_init_sta();
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
 }
 
 /* ---- */
 
 const esp_mqtt_client_config_t mqtt_cfg = {
-	.uri = MQTT_URI,
+    .uri = MQTT_URI,
 };
 
 static esp_mqtt_client_handle_t mqtt_client;
 
 void init_mqtt(void)
 {
-	ESP_LOGI(TAG, "Init MQTT");
-	mqtt_client = esp_mqtt_client_init(&mqtt_cfg);
-	// esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
-	esp_mqtt_client_start(mqtt_client);
+    ESP_LOGI(TAG, "Init MQTT");
+    mqtt_client = esp_mqtt_client_init(&mqtt_cfg);
+    // esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
+    esp_mqtt_client_start(mqtt_client);
 }
 
 /* ---- */
@@ -187,7 +187,7 @@ void init_mqtt(void)
 #define FLASHLIGHT_GPIO 4
 
 #define CAM_PIN_PWDN 32
-#define CAM_PIN_RESET -1	//software reset will be performed
+#define CAM_PIN_RESET -1    //software reset will be performed
 #define CAM_PIN_XCLK 0
 #define CAM_PIN_SIOD 26
 #define CAM_PIN_SIOC 27
@@ -207,200 +207,186 @@ void init_mqtt(void)
 #endif
 
 static camera_config_t camera_config = {
-	.pin_pwdn = CAM_PIN_PWDN,
-	.pin_reset = CAM_PIN_RESET,
-	.pin_xclk = CAM_PIN_XCLK,
-	.pin_sscb_sda = CAM_PIN_SIOD,
-	.pin_sscb_scl = CAM_PIN_SIOC,
+    .pin_pwdn = CAM_PIN_PWDN,
+    .pin_reset = CAM_PIN_RESET,
+    .pin_xclk = CAM_PIN_XCLK,
+    .pin_sscb_sda = CAM_PIN_SIOD,
+    .pin_sscb_scl = CAM_PIN_SIOC,
 
-	.pin_d7 = CAM_PIN_D7,
-	.pin_d6 = CAM_PIN_D6,
-	.pin_d5 = CAM_PIN_D5,
-	.pin_d4 = CAM_PIN_D4,
-	.pin_d3 = CAM_PIN_D3,
-	.pin_d2 = CAM_PIN_D2,
-	.pin_d1 = CAM_PIN_D1,
-	.pin_d0 = CAM_PIN_D0,
-	.pin_vsync = CAM_PIN_VSYNC,
-	.pin_href = CAM_PIN_HREF,
-	.pin_pclk = CAM_PIN_PCLK,
+    .pin_d7 = CAM_PIN_D7,
+    .pin_d6 = CAM_PIN_D6,
+    .pin_d5 = CAM_PIN_D5,
+    .pin_d4 = CAM_PIN_D4,
+    .pin_d3 = CAM_PIN_D3,
+    .pin_d2 = CAM_PIN_D2,
+    .pin_d1 = CAM_PIN_D1,
+    .pin_d0 = CAM_PIN_D0,
+    .pin_vsync = CAM_PIN_VSYNC,
+    .pin_href = CAM_PIN_HREF,
+    .pin_pclk = CAM_PIN_PCLK,
 
-	//XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
-	.xclk_freq_hz = 20000000,
-	.ledc_timer = LEDC_TIMER_0,
-	.ledc_channel = LEDC_CHANNEL_0,
+    //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
 
-	.pixel_format = PIXFORMAT_GRAYSCALE,	//YUV422,GRAYSCALE,RGB565,JPEG
-	.frame_size = FRAMESIZE_VGA,	//QQVGA-UXGA Do not use sizes above QVGA when not JPEG
+    .pixel_format = PIXFORMAT_GRAYSCALE,    //YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_VGA,    //QQVGA-UXGA Do not use sizes above QVGA when not JPEG
 
-	.jpeg_quality = 12,	//0-63 lower number means higher quality
-	.fb_count = 1		//if more than one, i2s runs in continuous mode. Use only with JPEG
+    .jpeg_quality = 12,    //0-63 lower number means higher quality
+    .fb_count = 1        //if more than one, i2s runs in continuous mode. Use only with JPEG
 };
 
 static esp_err_t init_camera()
 {
-	//initialize the camera
-	esp_err_t err = esp_camera_init(&camera_config);
-	if (err != ESP_OK) {
-		ESP_LOGE(TAG, "Camera Init Failed");
-		return err;
-	}
+    //initialize the camera
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Camera Init Failed");
+        return err;
+    }
 
-	return ESP_OK;
+    return ESP_OK;
 }
 
 /* ---- */
 
 void init_gpio()
 {
-	gpio_pad_select_gpio(BLINK_GPIO);
-	gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    gpio_pad_select_gpio(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
-	// gpio_pad_select_gpio(FLASHLIGHT_GPIO);
-	// gpio_set_direction(FLASHLIGHT_GPIO, GPIO_MODE_OUTPUT);
-	// gpio_set_level(FLASHLIGHT_GPIO, 0);  // off
+    // gpio_pad_select_gpio(FLASHLIGHT_GPIO);
+    // gpio_set_direction(FLASHLIGHT_GPIO, GPIO_MODE_OUTPUT);
+    // gpio_set_level(FLASHLIGHT_GPIO, 0);  // off
 }
 
 /* ---- */
 
 void time_sync_notification_cb(struct timeval *tv)
 {
-	ESP_LOGI(TAG, "Notification of a time synchronization event");
+    ESP_LOGI(TAG, "Notification of a time synchronization event");
 }
 
 static void initialize_sntp(void)
 {
-	ESP_LOGI(TAG, "Initializing SNTP");
-	sntp_setoperatingmode(SNTP_OPMODE_POLL);
-	sntp_setservername(0, "pool.ntp.org");
-	sntp_set_time_sync_notification_cb(time_sync_notification_cb);
-	sntp_init();
+    ESP_LOGI(TAG, "Initializing SNTP");
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, "pool.ntp.org");
+    sntp_set_time_sync_notification_cb(time_sync_notification_cb);
+    sntp_init();
 }
 
 static void sync_time(void)
 {
-	initialize_sntp();
+    initialize_sntp();
 
-	// wait for time to be set
-	// time_t now = 0;
-	// struct tm timeinfo = { 0 };
-	int retry = 0;
-	const int retry_count = 10;
-	while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET
-	       && ++retry < retry_count) {
-		ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)",
-			 retry, retry_count);
-		vTaskDelay(2000 / portTICK_PERIOD_MS);
-	}
-	// time(&now);
-	// setenv("TZ", "UTC-9", 1);
-	// tzset();
-	// localtime_r(&now, &timeinfo);
+    // wait for time to be set
+    // time_t now = 0;
+    // struct tm timeinfo = { 0 };
+    int retry = 0;
+    const int retry_count = 20;
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count) {
+        ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+    // time(&now);
+    // setenv("TZ", "UTC-9", 1);
+    // tzset();
+    // localtime_r(&now, &timeinfo);
 }
 
 /* ---- */
 
 void draw_info_string(uint8_t * fb_buf, const char *str)
 {
-	// VGA
-	int cam_w = 640;
-	int cam_h = 480;
+    // VGA
+    int cam_w = 640;
+    int cam_h = 480;
 
-	set_defaultfont();
-	draw_string(fb_buf, cam_w, cam_h, 8 - 1, 8, str, FONTCOLOR_BLACK);
-	draw_string(fb_buf, cam_w, cam_h, 8 + 1, 8, str, FONTCOLOR_BLACK);
-	draw_string(fb_buf, cam_w, cam_h, 8, 8 - 1, str, FONTCOLOR_BLACK);
-	draw_string(fb_buf, cam_w, cam_h, 8, 8 + 1, str, FONTCOLOR_BLACK);
-	draw_string(fb_buf, cam_w, cam_h, 8, 8, str, FONTCOLOR_WHITE);
+    set_defaultfont();
+    draw_string(fb_buf, cam_w, cam_h, 8 - 1, 8, str, FONTCOLOR_BLACK);
+    draw_string(fb_buf, cam_w, cam_h, 8 + 1, 8, str, FONTCOLOR_BLACK);
+    draw_string(fb_buf, cam_w, cam_h, 8, 8 - 1, str, FONTCOLOR_BLACK);
+    draw_string(fb_buf, cam_w, cam_h, 8, 8 + 1, str, FONTCOLOR_BLACK);
+    draw_string(fb_buf, cam_w, cam_h, 8, 8, str, FONTCOLOR_WHITE);
 }
 
 /* ---- */
 
 void app_main(void)
 {
-	bool is_wifi_connected = false;
+    bool is_wifi_connected = false;
 
-	init_gpio();
-	gpio_set_level(BLINK_GPIO, 0);	// on
+    init_gpio();
+    gpio_set_level(BLINK_GPIO, 0);
 
-	// ++take_count;
-	// ESP_LOGI(TAG, "Boot count: %d", take_count);
+	if (countdown > 0) {
+		countdown -= 1;
+		goto deepsleep;
+	}
+
+	ESP_LOGI(TAG, "Connecting to WiFi and getting time over NTP.");
+	init_wifi();
+	is_wifi_connected = true;
+
+	setenv("TZ", "UTC-9", 1);    // means UTC+9
+    tzset();
+
+	sync_time();
 
 	time_t now;
-	struct tm timeinfo;
+    struct tm timeinfo;
 	time(&now);
-	setenv("TZ", "UTC-9", 1);	// TODO: means UTC+9
-	tzset();
 	localtime_r(&now, &timeinfo);
-	if (timeinfo.tm_year < (2016 - 1900)) {
-		ESP_LOGI(TAG,
-			 "Time is not set yet. Connecting to WiFi and getting time over NTP.");
-		init_wifi();
-		is_wifi_connected = true;
-		sync_time();
-		// update 'now' variable with current time
-		time(&now);
-		localtime_r(&now, &timeinfo);
+
+	countdown = 23 - timeinfo.tm_hour + 12;
+
+    char strftime_buf[32];
+    strftime(strftime_buf, sizeof(strftime_buf), "%Y-%m-%d(%a) %T", &timeinfo);
+    ESP_LOGI(TAG, "The current date/time is: %s", strftime_buf);
+
+    // gpio_set_level(FLASHLIGHT_GPIO, 1);  // on
+    ESP_LOGI(TAG, "Taking picture...");
+    init_camera();
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+    camera_fb_t *fb;
+    fb = esp_camera_fb_get();
+
+    char strinfo_buf[64];
+    sprintf(strinfo_buf, "%s cnt: %03d", strftime_buf, ++seq_num);
+    draw_info_string(fb->buf, strinfo_buf);
+
+    uint8_t *buf = NULL;
+    size_t buf_len = 0;
+    bool converted = frame2jpg(fb, 80, &buf, &buf_len);
+    esp_camera_fb_return(fb);
+    esp_camera_deinit();
+
+	if (!converted) {
+        goto deepsleep;
 	}
 
-	// only take snapshot in 11 and 12 o'clock
-	// it makes -at last- one picture taken at a day.
-	if (take_count > 0 && timeinfo.tm_hour != 12) {
-		ESP_LOGI(TAG, "Skip take picture");
-		goto deepsleep;
-	}
+    if (!is_wifi_connected) {
+        init_wifi();
+        is_wifi_connected = true;
+    }
 
-	char strftime_buf[32];
-	strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
-	ESP_LOGI(TAG, "The current date/time is: %s", strftime_buf);
-
-	// gpio_set_level(FLASHLIGHT_GPIO, 1);  // on
-	ESP_LOGI(TAG, "Taking picture...");
-	init_camera();
-	vTaskDelay(5000 / portTICK_PERIOD_MS);
-
-	camera_fb_t *fb;
-	// for (int i = 0; i < 5; i++) {
-	//      fb = esp_camera_fb_get();
-	//      esp_camera_fb_return(fb);
-	// }
-	fb = esp_camera_fb_get();
-	// gpio_set_level(FLASHLIGHT_GPIO, 0);  // off
-
-	char strinfo_buf[64];
-	sprintf(strinfo_buf, "%s cnt: %03d", strftime_buf, ++take_count);
-	draw_info_string(fb->buf, strinfo_buf);
-
-	uint8_t *buf = NULL;
-	size_t buf_len = 0;
-	bool converted = frame2jpg(fb, 80, &buf, &buf_len);
-	esp_camera_fb_return(fb);
-	esp_camera_deinit();
-	if (!converted)
-		goto deepsleep;
-
-	if (!is_wifi_connected) {
-		init_wifi();
-		is_wifi_connected = true;
-	}
-
-	init_mqtt();
-	ESP_LOGI(TAG, "Sending it to MQTT topic %s ...", MQTT_TOPIC);
-	esp_mqtt_client_publish(mqtt_client, MQTT_TOPIC,
-				(const char *)(buf), buf_len, 0, 0);
-	if (take_count > 1) {
-		sync_time();	// interal RTC sucks sync time again.
-	}
+    init_mqtt();
+    ESP_LOGI(TAG, "Sending it to MQTT topic %s ...", MQTT_TOPIC);
+    esp_mqtt_client_publish(mqtt_client, MQTT_TOPIC, (const char *)(buf), buf_len, 0, 0);
 
  deepsleep:
-	// TODO: need free buf?
-	gpio_set_level(BLINK_GPIO, 1);	// off
+    // TODO: need free buf?
+    gpio_set_level(BLINK_GPIO, 1);
 
-	esp_mqtt_client_destroy(mqtt_client);
-	if (is_wifi_connected)
-		esp_wifi_stop();
+    esp_mqtt_client_destroy(mqtt_client);
+    if (is_wifi_connected) {
+        esp_wifi_stop();
+	}
 
-	const int deep_sleep_sec = 1 * 60 * 60;
-	ESP_LOGI(TAG, "Entering deep sleep for %d seconds", deep_sleep_sec);
-	esp_deep_sleep(1000000LL * deep_sleep_sec);
+    const int deep_sleep_sec = 1 * 60 * 60;
+    ESP_LOGI(TAG, "Entering deep sleep for %d seconds, cnt: %d", deep_sleep_sec, countdown);
+    esp_deep_sleep(1000000LL * deep_sleep_sec);
 }
